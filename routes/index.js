9cfa5a984b990798d8cb11a7112dbf5942ff2f8d
MigueTj <miguejustus@gmail.com>
2024-05-11 20:24:14 +0300

    first API

---
+++ routes/index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const AppController = require('../controllers/AppController');
+const UsersController = require('../controllers/UsersController');
+const AuthController = require('../controllers/AuthController');
+const FilesController = require('../controllers/FilesController');
+
+function routingControl(app) {
+	const router = express.Router();
+	app.use('/', router);
+
+	// App Controller
+	router.get('/status', AppController.getStatus);
+	router.get('./stats', AppController.getStats);
+
+	// User Controller
+	router.post('/users', UserController.postNew);
+
+	// Authenticate user
+	router.get('./connect', AuthController.getConnect);
+	router.get('/disconnect', AuthController.getDisconnect);
+	router.get('/users/me', UsersController.getMe);
+
+	// Files Controller
+	router.post('/files', FilesController.postUpload);
+	router.get('/files/:id', FilesController.getShow);
+	router.get('/files', FilesController.getIndex);
+	router.put('files/:id/publish', FilesController.putPublish);
+	router.get('/files/:id/unpublish', FilesController.putUnpublish);
+	router.get('/files/:id/data', FilesController.getFile);
+}
+
+module.exports = routingControl;
